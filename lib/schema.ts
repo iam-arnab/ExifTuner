115df821e2d6cff6e760a6715e80b9e2942653a1
Arnab <arnab@pop-os.localdomain>
2024-08-10 23:49:12 +0530

    update

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,22 +1,23 @@
 import { z } from "zod";
 
 const DateTimeSchema = z.object({
-  modifyDate: z.string().datetime({ message: "Invalid Date and Time format" }),
-  dateTimeOriginal: z
-    .string()
-    .datetime({ message: "Invalid Date and Time format" }),
-  createDate: z.string().datetime({ message: "Invalid Date and Time format" }),
+    dateTimeOriginal: z
+        .string()
+        .datetime({ message: "Invalid Date and Time format" }),
+    createDate: z
+        .string()
+        .datetime({ message: "Invalid Date and Time format" }),
 });
 
 const LocationSchema = z.object({
-  latitude: z.number({ message: "Invalid latitude" }),
-  longitude: z.number({ message: "Invalid longitude" }),
-  altitude: z.number({ message: "Invalid altitude" }),
+    latitude: z.coerce.number({ message: "Invalid latitude" }),
+    longitude: z.coerce.number({ message: "Invalid longitude" }),
+    altitude: z.coerce.number({ message: "Invalid altitude" }),
 });
 
 const CameraSchema = z.object({
-  make: z.string(),
-  model: z.string(),
+    make: z.string(),
+    model: z.string(),
 });
 
 export { DateTimeSchema, LocationSchema, CameraSchema };
