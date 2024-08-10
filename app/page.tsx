52eb86cece19ab5ce06b07a3817bbe7dd6850d8f
Arnab <arnab@pop-os.localdomain>
2024-08-10 23:46:12 +0530

    refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,15 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import DateTimeForm from "@/components/ui/dateTimeForm";
-import LocationForm from "@/components/ui/locationForm";
-import CameraForm from "@/components/ui/cameraForm";
 import ImageForm from "@/components/ui/imageForm";
 
 export default function Home() {
-  return (
-    <main className="relative">
-      <div className="absolute top-2 right-2">
-        <ModeToggle />
-      </div>
-      <div className="flex items-center justify-center h-screen">
-        <Tabs defaultValue="d&t">
-          <ImageForm />
-          <div className="m-2">
-            <TabsList>
-              <TabsTrigger value="d&t">Date & Time</TabsTrigger>
-              <TabsTrigger value="location">Location</TabsTrigger>
-              <TabsTrigger value="camera">Camera</TabsTrigger>
-            </TabsList>
-            <TabsContent value="d&t">
-              <DateTimeForm />
-            </TabsContent>
-            <TabsContent value="location">
-              <LocationForm />
-            </TabsContent>
-            <TabsContent value="camera">
-              <CameraForm />
-            </TabsContent>
-          </div>
-        </Tabs>
-      </div>
-    </main>
-  );
+    return (
+        <main className="relative">
+            <div className="absolute top-2 right-2">
+                <ModeToggle />
+            </div>
+            <div className="flex items-center justify-center h-screen">
+                <ImageForm />
+            </div>
+        </main>
+    );
 }
--- components/ui/imageForm.tsx
+++ components/ui/imageForm.tsx
@@ -30,4 +30,34 @@ export default function ImageForm() {
       <Input type="file" onChange={handleChange} />
     </>
   );
+    return (
+        <Tabs defaultValue="d&t">
+            {image && (
+                <Image
+                    src={image}
+                    alt="image"
+                    width={300}
+                    height={300}
+                    className="m-2"
+                />
+            )}
+            <Input type="file" onChange={handleChange} />
+            <div className="m-2">
+                <TabsList>
+                    <TabsTrigger value="d&t">Date & Time</TabsTrigger>
+                    <TabsTrigger value="location">Location</TabsTrigger>
+                    <TabsTrigger value="camera">Camera</TabsTrigger>
+                </TabsList>
+                <TabsContent value="d&t">
+                    <DateTimeForm dataUrl={image as string} />
+                </TabsContent>
+                <TabsContent value="location">
+                    <LocationForm dataUrl={image as string} />
+                </TabsContent>
+                <TabsContent value="camera">
+                    <CameraForm dataUrl={image as string} />
+                </TabsContent>
+            </div>
+        </Tabs>
+    );
 }
