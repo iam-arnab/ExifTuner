7c1df349993ccbcbd7039c14fb33e59c5eed7e81
Arnab <arnab@pop-os.localdomain>
2024-08-11 16:28:31 +0530

    adding credit

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,12 @@ export default function Home() {
             <div className="m-10 flex flex-col items-center justify-center min-h-screen">
                 <Logo />
                 <ImageForm />
+                <p className="mt-20">
+                    Made with ❤️ by{' '}
+                    <a href="https://bio.link/arnab" className="font-bold">
+                        Arnab
+                    </a>
+                </p>
             </div>
         </main>
     );
